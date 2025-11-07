c53f55e6fd957e18ba4edb4dbbab798c084a3914
guicoelhodev <gs.coelho_dev@outlook.com>
2025-11-07 15:28:29 -0300

    feat: add title, image and previous languages used

--- main.js
+++ main.js
@@ -1,14 +1,43 @@
 console.log('File was loaded')
 
 /** @function getProjects
-	* @returns {Array<{ title: string }>}
+	* @returns {Array<{ 
+	* 	title: string,
+	* 	src: string,
+	* 	href: string
+	* 	languages: Array<string>
+	* 	}>}
 	*/
 function getProjects() {
 
 	return [
-		{ title: "Project 1" },
-		{ title: "Project 2" },
-		{ title: "projects 3" }
+		{
+			title: "Pomodoro lofi",
+			src: "./assets/radio_lofi.png",
+			href: "https://radio.guicoelhodev.com/",
+			languages: [
+				'lni lni-svelte text-2xl text-orange-600',
+				'lni lni-svelte text-2xl text-orange-600'
+			]
+		},
+		{
+			title: "React Music",
+			src: "./assets/react_music.png",
+			href: "https://react-music-xi.vercel.app/",
+			languages: ['lni lni-svelte text-2xl text-orange-600']
+		},
+		{
+			title: "Pokedex",
+			src: "./assets/pokedex.png",
+			href: "https://pokedex.guicoelhodev.com/",
+			languages: ['lni lni-svelte text-2xl text-orange-600']
+		},
+		{
+			title: "Discord clone",
+			src: "./assets/discord_ui_clone.png",
+			href: "https://pokedex.guicoelhodev.com/",
+			languages: ['lni lni-svelte text-2xl text-orange-600']
+		}
 	]
 }
 
